9aa7c23255e7add3784254625720e109d6e60933
fortime <palfortime@gmail.com>
2022-08-17 00:18:21 +0800

    chore: update doc

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# Introduction
+# tracing-opentelemetry-fmt
+
+> A library for adding `trace.id` and `span.id` to tracing log
+
+## About
+
 Implement a `Layer` of `tracing-subscriber` which combines a `OpentelmetryLayer` and `FmtLayer`. This layer adds `trace.id` and `span.id` to `FmtLayer` so that we can log `trace.id` and `span.id` from opentelemetry.
+
+## Examples
+
+```
+use std::io::{self, Write};
+
+use opentelemetry::sdk::export::trace::stdout;
+use tracing_subscriber::{
+    fmt::{self, format::FmtSpan},
+    layer::SubscriberExt,
+    util::SubscriberInitExt,
+};
+use tracing_opentelemetry_fmt::OpenTelemetryFmtLayerBuilder;
+
+fn main() {
+    let fmt_layer = fmt::layer()
+        .with_thread_ids(true)
+        .with_target(true)
+        .with_span_events(FmtSpan::FULL);
+    let tracer = stdout::new_pipeline().with_writer(io::sink()).install_simple();
+    let opentelemetry_layer = tracing_opentelemetry::layer()
+        .with_exception_field_propagation(true)
+        .with_threads(true)
+        .with_tracer(tracer);
+
+    let opentelemetry_fmt_layer =
+        OpenTelemetryFmtLayerBuilder::new(opentelemetry_layer, fmt_layer).build();
+    tracing_subscriber::registry()
+        .with(opentelemetry_fmt_layer)
+        .try_init()
+        .expect("It should be successful");
+
+    tracing::info_span!("span1").in_scope(|| {
+        tracing::info!("in span1");
+    });
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,46 @@
+//! # tracing-opentelemetry-fmt
+//!
+//! > A library for adding `trace.id` and `span.id` to tracing log
+//!
+//! ## About
+//! Implement a `Layer` of `tracing-subscriber` which combines a `OpentelmetryLayer` and `FmtLayer`. This layer adds `trace.id` and `span.id` to `FmtLayer` so that we can log `trace.id` and `span.id` from opentelemetry.
+//!
+//! ## Examples
+//!
+//! ```
+//! use std::io::{self, Write};
+//!
+//! use opentelemetry::sdk::export::trace::stdout;
+//! use tracing_subscriber::{
+//!     fmt::{self, format::FmtSpan},
+//!     layer::SubscriberExt,
+//!     util::SubscriberInitExt,
+//! };
+//! use tracing_opentelemetry_fmt::OpenTelemetryFmtLayerBuilder;
+//!
+//! fn main() {
+//!     let fmt_layer = fmt::layer()
+//!         .with_thread_ids(true)
+//!         .with_target(true)
+//!         .with_span_events(FmtSpan::FULL);
+//!     let tracer = stdout::new_pipeline().with_writer(io::sink()).install_simple();
+//!     let opentelemetry_layer = tracing_opentelemetry::layer()
+//!         .with_exception_field_propagation(true)
+//!         .with_threads(true)
+//!         .with_tracer(tracer);
+//!
+//!     let opentelemetry_fmt_layer =
+//!         OpenTelemetryFmtLayerBuilder::new(opentelemetry_layer, fmt_layer).build();
+//!     tracing_subscriber::registry()
+//!         .with(opentelemetry_fmt_layer)
+//!         .try_init()
+//!         .expect("It should be successful");
+//!
+//!     tracing::info_span!("span1").in_scope(|| {
+//!         tracing::info!("in span1");
+//!     });
+//! }
+//! ```
 use std::any::TypeId;
 
 use opentelemetry::trace::{TraceContextExt, Tracer};
@@ -153,51 +196,4 @@ where
 
 #[cfg(test)]
 mod tests {
-    use std::io::Write;
-
-    use opentelemetry::sdk::export::trace::stdout;
-    use tracing_subscriber::{
-        fmt::{self, format::FmtSpan},
-        layer::SubscriberExt,
-        util::SubscriberInitExt,
-    };
-
-    use super::*;
-
-    #[derive(Debug)]
-    struct Drain;
-
-    impl Write for Drain {
-        fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-            Ok(buf.len())
-        }
-
-        fn flush(&mut self) -> std::io::Result<()> {
-            Ok(())
-        }
-    }
-
-    #[test]
-    fn it_works() {
-        let fmt_layer = fmt::layer()
-            .with_thread_ids(true)
-            .with_target(true)
-            .with_span_events(FmtSpan::FULL);
-        let tracer = stdout::new_pipeline().with_writer(Drain).install_simple();
-        let opentelemetry_layer = tracing_opentelemetry::layer()
-            .with_exception_field_propagation(true)
-            .with_threads(true)
-            .with_tracer(tracer);
-
-        let opentelemetry_fmt_layer =
-            OpenTelemetryFmtLayerBuilder::new(opentelemetry_layer, fmt_layer).build();
-        tracing_subscriber::registry()
-            .with(opentelemetry_fmt_layer)
-            .try_init()
-            .expect("It should be successful");
-
-        tracing::info_span!("span1").in_scope(|| {
-            tracing::info!("in span1");
-        });
-    }
 }
